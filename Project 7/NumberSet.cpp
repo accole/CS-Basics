4f7733ae28f1f13652883f62baa160b6ff7d87f7
Adam Cole <39781606+accole@users.noreply.github.com>
2019-01-27 18:14:14 -0800

    Add files via upload

---
+++ Project 7/NumberSet.cpp
@@ -0,0 +1,72 @@
+//
+//  NumberSet.cpp
+//  SudokuGame
+//
+//  Created by Howard Stahl on 11/5/17.
+//  Copyright © 2017 Howard Stahl. All rights reserved.
+//
+
+#include "NumberSet.h"
+
+namespace cs31
+{
+	NumberSet::NumberSet()
+	{
+		/* As with the Sudoku board, zero represents an unset value */
+		mOne = 0;
+		mTwo = 0;
+		mThree = 0;
+		mFour = 0;
+		mFive = 0;
+		mSix = 0;
+		mSeven = 0;
+		mEight = 0;
+	}
+
+	NumberSet::NumberSet(int one, int two, int three,
+		int four, int five, int six,
+		int seven, int eight)
+	{
+		mOne = one;
+		mTwo = two;
+		mThree = three;
+		mFour = four;
+		mFive = five;
+		mSix = six;
+		mSeven = seven;
+		mEight = eight;
+	}
+
+	/* turn this set into string */
+	std::string NumberSet::stringify() const
+	{
+		std::string s;
+		s = "SET-";
+		s += std::to_string(mOne);
+		s += std::to_string(mTwo);
+		s += std::to_string(mThree);
+		s += std::to_string(mFour);
+		s += std::to_string(mFive);
+		s += std::to_string(mSix);
+		s += std::to_string(mSeven);
+		s += std::to_string(mEight);
+		return(s);
+	}
+
+	/* return true if the value argument is not                */
+	/* one of the eight values found in this NumberSet already */
+	/* return false otherwise                                  */
+	/* TODO: CS31 Students must implement this operation */
+	bool NumberSet::notFound(int value) const
+	{
+		std::string numset = this->NumberSet::stringify();
+		std::string valuex = std::to_string(value);
+		char c = valuex[0];
+		for (size_t i = 4; i < numset.size(); i++) {
+			if (numset[i] == c) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
